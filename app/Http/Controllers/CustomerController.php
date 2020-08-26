d3a74fdeb6c32276a60545ff3b6678e26ce3f579
Алексей Анисимов <ork.04@yandex.ru>
2020-08-26 19:15:01 +0300

    Просмотр клиента

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -3,8 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Customer;
 
 class CustomerController extends Controller
 {
-    //
+    public function show($id)
+    {
+        $data = [
+            'customer' => null
+        ];
+        $phones = [];
+        $emails = [];
+
+        $customer = Customer::find($id);
+
+        if (is_null($customer)) {
+            return response($data, 404);
+        }
+
+        $customer->first();
+
+        foreach ($customer->customerPhones()->get() as $phone) {
+            $phones[] = $phone->phone;
+        }
+
+        foreach ($customer->customerEmails()->get() as $email) {
+            $emails[] = $email->email;
+        }
+
+        $data = [
+            'customer' => [
+                'last_name' => $customer->last_name,
+                'first_name' => $customer->first_name,
+                'phones' => $phones,
+                'emails' => $emails,
+            ]
+        ];
+
+        return response($data, 200);
+    }
 }
--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\CustomerPhone;
+use App\Models\CustomerEmail;
 
 class Customer extends Model
 {
     protected $table = 'customer';
+
+    public function customerPhones()
+    {
+        return $this->hasMany(CustomerPhone::class);
+    }
+
+    public function customerEmails()
+    {
+        return $this->hasMany(CustomerEmail::class);
+    }
 }
