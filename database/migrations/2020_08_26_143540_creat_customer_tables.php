a3dceaa1a75f5671cc188c0087b18b5acf1592b9
Алексей Анисимов <ork.04@yandex.ru>
2020-08-26 17:55:20 +0300

    Таблицы создания клиента

---
+++ database/migrations/2020_08_26_143540_creat_customer_tables.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatCustomerTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('last_name',255);
+            $table->string('first_name',255);
+            $table->timestamps();
+        });
+
+        Schema::create('customer_phone', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->integer('customer_id');
+            $table->decimal('phone', 15, 0);
+        });
+
+        Schema::create('customer_email', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->integer('customer_id');
+            $table->string('email', '255');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
